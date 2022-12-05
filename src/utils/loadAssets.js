51759f28198f172efe7064ffe0210a7db04f75e3
later_zc <2363442097@qq.com>
2022-12-05 19:59:00 +0800

    docs:update

--- src/utils/loadAssets.js
+++ src/utils/loadAssets.js
@@ -4,7 +4,5 @@ export const getAssetsUrl = (img) => {
   // 参数二：当前模块(文件)所在路径
   // https://cn.vitejs.dev/guide/assets.html#new-url-url-import-meta-url
   // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/import.meta
-  // return new URL(`../assets/img${img}`, import.meta.url).href 
-  // 上面写法不行 咱也不知道为啥 就必须得跟着官方来呗 img后面加/
   return new URL(`../assets/img/${img}`, import.meta.url).href
 }
